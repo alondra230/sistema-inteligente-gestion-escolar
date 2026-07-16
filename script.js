b06c623bd1613ef93a56e4746815bb8bcab09c02
alondra230 <rodasalondra10@gmail.com>
2026-07-16 11:28:00 -0600

    Aprobado script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const formulario = document.getElementById("formulario");
 
+
 if(formulario){
+
 
 formulario.addEventListener("submit", function(e){
 
+
 e.preventDefault();
+
 
 
 const estudiante = {
@@ -13,9 +17,21 @@ nombre: document.getElementById("nombre").value,
 
 edad: document.getElementById("edad").value,
 
-grado: document.getElementById("grado").value,
+dpi: document.getElementById("dpi").value,
 
 direccion: document.getElementById("direccion").value,
+
+genero: document.getElementById("genero").value,
+
+carrera: document.getElementById("carrera").value,
+
+pais: document.getElementById("pais").value,
+
+departamento: document.getElementById("departamento").value,
+
+municipio: document.getElementById("municipio").value,
+
+grado: document.getElementById("grado").value,
 
 telefono: document.getElementById("telefono").value,
 
@@ -31,10 +47,14 @@ nota3: Number(document.getElementById("nota3").value)
 
 
 
-const promedio = ((estudiante.nota1 + estudiante.nota2 + estudiante.nota3) / 3).toFixed(1);
+const promedio = (
+(estudiante.nota1 + estudiante.nota2 + estudiante.nota3) / 3
+).toFixed(2);
+
 
 
 estudiante.promedio = promedio;
+
 
 
 if(promedio >= 61){
@@ -49,19 +69,25 @@ estudiante.estado = "Reprobado ❌";
 
 
 
+
 let estudiantes = JSON.parse(localStorage.getItem("estudiantes")) || [];
 
 
 estudiantes.push(estudiante);
 
 
-localStorage.setItem("estudiantes", JSON.stringify(estudiantes));
+localStorage.setItem(
+"estudiantes",
+JSON.stringify(estudiantes)
+);
+
 
 
 alert("Estudiante registrado correctamente");
 
 
 formulario.reset();
+
 
 
 });
@@ -72,7 +98,9 @@ formulario.reset();
 
 
 
-const tabla = document.querySelector("#tabla tbody");
+
+const tabla = document.querySelector("#tablaDatos");
+
 
 
 if(tabla){
@@ -81,29 +109,33 @@ if(tabla){
 let estudiantes = JSON.parse(localStorage.getItem("estudiantes")) || [];
 
 
+
 estudiantes.forEach(function(estudiante){
 
 
-const fila = document.createElement("tr");
+
+let fila = document.createElement("tr");
+
 
 
 fila.innerHTML = `
 
 <td>${estudiante.nombre}</td>
 
-<td>${estudiante.grado}</td>
+<td>${estudiante.dpi}</td>
+
+<td>${estudiante.carrera}</td>
 
 <td>${estudiante.promedio}</td>
 
 <td>${estudiante.estado}</td>
 
-<td>${estudiante.telefono}</td>
+<td>${estudiante.municipio}</td>
 
 <td>${estudiante.correo}</td>
 
-<td>${estudiante.direccion}</td>
-
 `;
+
 
 
 tabla.appendChild(fila);
